c513619297ff486fd06ac68f673e3c75c49a4ea3
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-04-28 21:30:07 +0200

    Ladeni

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-
+/* 
     const colorBoxes = document.querySelectorAll(".color-box");
     const productImage = document.getElementById("product-image");
 
     colorBoxes.forEach(colorBox => {
         colorBox.addEventListener("click", () => {
+
+            // vezme vybranou třídu a obraví to podle názvu třídy, tedy podle základní barvy white/black/red...
 
             const color = colorBox.classList[1];
             productImage.style.fill = color;
@@ -14,4 +16,29 @@
 
                 box.classList.add("vybrana");
         });
+    }); */
+
+
+    const colorBoxes = document.querySelectorAll(".color-box");
+    const productImage = document.getElementById("product-image");
+    
+    colorBoxes.forEach(colorBox => {
+        colorBox.addEventListener("click", () => {
+    
+            //způsob jedna - vezme barvu přímo z html
+            const color = colorBox.style.backgroundColor;
+            productImage.style.fill = color;
+    
+            // způsob dva - využije barvu ze stylů s třídy .white/...
+    /*         const color = window.getComputedStyle(colorBox).getPropertyValue("background-color"); 
+            productImage.style.fill = color; */
+    
+
+            colorBoxes.forEach(box => {
+                box.classList.remove("vybrana");
+            });
+    
+            colorBox.classList.add("vybrana");
+        });
     });
+    
