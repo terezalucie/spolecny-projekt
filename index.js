bb916e0c8ba12dd41e037ad29288ee5c423e7f95
Alena Holubova <aja.holubova@email.cz>
2024-04-28 20:25:45 +0200

    doplneni JS

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-console.log('funguju');
 
+    const colorBoxes = document.querySelectorAll(".color-box");
+    const productImage = document.getElementById("product-image");
 
+    colorBoxes.forEach(colorBox => {
+        colorBox.addEventListener("click", () => {
 
+            const color = colorBox.classList[1];
+            productImage.style.fill = color;
 
+            colorBoxes.forEach(box => {
+                box.classList.remove("vybrana");
+            });
+
+                box.classList.add("vybrana");
+        });
+    });
